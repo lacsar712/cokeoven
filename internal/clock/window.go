11c5698a156473c24737e3bc107904dfe37dc047
lacsar712 <lacsar712@users.noreply.github.com>
2026-08-24 21:12:58 +0800

    init: cokeoven

---
+++ internal/clock/window.go
@@ -0,0 +1,30 @@
+package clock
+
+import (
+	"time"
+
+	"github.com/lacsar712/cokeoven/internal/model"
+)
+
+type CarbonWindow struct {
+	clk      Clock
+	duration time.Duration
+}
+
+func NewCarbonWindow(clk Clock, duration time.Duration) *CarbonWindow {
+	if duration <= 0 {
+		duration = 2 * time.Minute
+	}
+	return &CarbonWindow{clk: clk, duration: duration}
+}
+
+func (w *CarbonWindow) Active(anchor time.Time) bool {
+	return WindowElapsed(w.clk, anchor, w.duration)
+}
+
+func (w *CarbonWindow) Require(anchor time.Time) error {
+	if w.Active(anchor) {
+		return nil
+	}
+	return model.ErrCarbonHold
+}
